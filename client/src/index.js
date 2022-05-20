d7e71c884b5b74a887f27291c6da5922e5a567c3
Vikrant Guleria <vguleria1108@gmail.com>
2022-05-20 18:58:36 +0530

    Route setup using react router dom v6

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginScreen from "./pages/index"
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <Routes>
+      <Route path={`/`} exact element={<LoginScreen/>} />
+    </Routes>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
