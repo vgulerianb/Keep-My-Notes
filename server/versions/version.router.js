54e1f2fe67c3d28315f852fb6d6ca1db533424ab
Vikrant Guleria <vguleria1108@gmail.com>
2022-05-21 15:56:31 +0530

    Get, Post, Put Notes api without authorization addede

--- server/versions/version.router.js
+++ server/versions/version.router.js
@@ -3,11 +3,31 @@ const router = express.Router();
 const NotesModal = require("../models/notes.model"); 
 
 router.get("/notes", async (req, res) => {
-    const test = await NotesModal.find()
+    const test = await NotesModal.find({user: "vguleria1108@gmail.com"})
     console.log({test})
-    res.status(200).send({ status: "success", message: "API is working fine." });
+    res.status(200).send({ status: "success", message: "API is working fine." ,test});
 });
 
+router.post("/notes", async (req, res) => {
+    const params = req.params;
+    const queryParams = req.query;
+    const bodyParams = req.body;
+    const request = { ...params, ...queryParams, ...bodyParams };
+    console.log({request})
+    const queryInfo = await NotesModal.create(request)
+    res.status(200).send({ status: "success", message: "API is working fine." ,queryInfo});
+});
+
+router.put("/notes", async (req, res) => {
+    const params = req.params;
+    const queryParams = req.query;
+    const bodyParams = req.body;
+    const request = { ...params, ...queryParams, ...bodyParams };
+    console.log({request})
+    const queryInfo = await NotesModal.updateOne({_id: request['id']},{title: request['title'], description: request['description']})
+    res.status(200).send({ status: "success", message: "API is working fine." ,queryInfo});
+});
+ 
 router.get("/", function (req, res) {
     res.status(200).send({ status: "success", message: "API is working fine." });
 });
